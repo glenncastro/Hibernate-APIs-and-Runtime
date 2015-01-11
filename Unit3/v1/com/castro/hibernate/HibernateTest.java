91a4681ff6be24adfc1557d90ab495b9a864a3a8
Glenn Castro <glenn.castro@gmail.com>
2015-01-11 14:39:41 +1100

    Unit3 - Version 1 - Cacheing in Hibernate (1st Level)

---
+++ Unit3/v1/com/castro/hibernate/HibernateTest.java
@@ -0,0 +1,44 @@
+package com.castro.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.castro.dto.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+			.applySettings(configuration.getProperties());
+		SessionFactory factory = configuration.buildSessionFactory(builder.build());
+		
+		Session session = factory.openSession();
+		session.beginTransaction();
+		
+		// Version 1 - First level cache - (Session)
+		// Result: Only one query will run
+		UserDetails user = (UserDetails) session.get(UserDetails.class, 1);
+		user.setUserName("User 01");
+		session.save(user);
+		// Version 1
+		//UserDetails user2 = (UserDetails) session.get(UserDetails.class, 1);
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		// Version 2 - First level cache
+		// Different sessions, two queries will run
+		Session session2 = factory.openSession();
+		session2.beginTransaction();
+		
+		UserDetails user2 = (UserDetails) session2.get(UserDetails.class, 1);
+		
+		session2.getTransaction().commit();
+		session2.close();
+	}
+
+}
